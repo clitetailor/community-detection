6e8fb8bf0118185916619d0aae3611db1d5a0c78
clitetailor <ducnhatphung@gmail.com>
2018-05-02 14:32:31 +0700

    backup data

--- src/followers_list/index.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-
-with open('./vnnlp.yaml', 'r') as input_file:
-    data = yaml.load(input_file)
-
-    print(len(data))
--- src/get_followers.py
+++ src/get_followers.py
@@ -11,27 +11,23 @@ def main():
     utils.ensure_dir('followers_list/vnnlp.yaml')
 
     next_cursor = None
-
     while next_cursor != 0 and next_cursor != '0':
         try:
             with open('followers_list/vnnlp.yaml', 'a') as output_file:
                 data = {'screen_name': 'vnnlp'}
-
                 if next_cursor:
                     data['cursor'] = next_cursor
-
                 r = api.request('followers/list', data)
 
+                response = r.json()
+                if 'next_cursor' in response:
+                    next_cursor = response['next_cursor']
+
                 for item in r:
+                    print(item['screen_name'])
                     yaml.dump([item], output_file)
 
-                    print(item['screen_name'])
-
-                    if 'next_cursor' in item:
-                        next_cursor = item['next_cursor']
-
                 time.sleep(60)
-
         except TwitterAPI.TwitterRequestError:
             time.sleep(60)
 
