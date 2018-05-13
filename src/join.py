f118acb477d315a7dc1514427c59abed53a635cd
clitetailor <ducnhatphung@gmail.com>
2018-05-13 12:10:47 +0700

    join files

---
+++ src/join.py
@@ -0,0 +1,43 @@
+import os
+import yaml
+
+
+def main():
+    final_list = []
+
+    files = os.listdir('friends_list')
+    for f in files:
+        user1 = os.path.splitext(os.path.basename(f))[0]
+
+        users = []
+        with open(os.path.join('friends_list', f), 'r') as input_file:
+            users = yaml.load(input_file)
+
+        if not isinstance(users, list):
+            continue
+
+        for user2 in users:
+            if not user1 == user2:
+                final_list.append([user1, user2])
+
+    files = os.listdir('followers_list')
+    for f in files:
+        user1 = os.path.splitext(os.path.basename(f))[0]
+
+        users = []
+        with open(os.path.join('followers_list', f), 'r') as input_file:
+            users = yaml.load(input_file)
+
+        if not isinstance(users, list):
+            continue
+
+        for user2 in users:
+            if not user1 == user2:
+                final_list.append([user2, user1])
+
+    with open('friends_list.yaml', 'w') as output_file:
+        yaml.dump(final_list, output_file)
+
+
+if __name__ == '__main__':
+    main()
